79208ae260ace725ca061400bbed10127b512070
Gabriel Bezerra Valério <gbrvalerio@gmail.com>
2020-01-24 09:43:31 -0300

    Take advantage of multithreading on image squaring
    believe me: its a lot faster

--- squarify-images.py
+++ squarify-images.py
@@ -1,16 +1,41 @@
 import glob
-import Image
+from PIL import Image
+from threading import Thread
 
 frame_size = (640, 640)
 directory = 'images'
 
-for frame in sorted(glob.glob(directory + '/*.png')):
-	# enlarge 640 x 480 frames up to frame_size 
-	# by filling the bottom area with whitespace
-	new_frame = Image.new('RGB', frame_size, 'white')
-	new_frame.paste(Image.open(frame), (0, 0))
+piclist = list(sorted(glob.glob(directory + '/*.png')))
 
-	new_frame_path = frame.replace('.png', '_squared.png')
-	new_frame.save(new_frame_path, 'png')
 
-	print('saved ' + new_frame_path)
+class WorkerThread(Thread):
+
+	def __init__ (self, name='Thread'):
+		Thread.__init__(self)
+		self.name = name
+
+	def run(self):		
+		while True:
+			actual_image = None
+			try:
+				actual_image = piclist.pop()
+			except:
+				print('[%s] no able to pop anymore...' % self.name)
+				return
+
+			new_frame = Image.new('RGB', frame_size, 'white')
+			new_frame.paste(Image.open(actual_image), (0, 0))
+
+			new_frame_path = actual_image.replace('.png', '_squared.png')
+			new_frame.save(new_frame_path, 'png')
+
+			print('[%s] saved %s' % (self.name, new_frame_path))
+		
+threads = []
+for i in range(10):
+  threads.append(
+    WorkerThread(name="Thread-%d" % (i + 1)),
+  )
+
+for thread in threads:
+  thread.start()
